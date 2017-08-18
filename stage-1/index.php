f40afc7d4005e6b3b8526ca459efc2f298a70595
johnotu <johnne01@gmail.com>
2017-08-18 20:52:12 +0000

    Connected to MYSQL db
    ... running on localhost:3306 and displayed oku table streets.
    Changes to be committed:
    	new file:   index.php

---
+++ stage-1/index.php
@@ -0,0 +1,17 @@
+<?php
+  $servername = "localhost:3306";
+  $username = "johnotu";
+  $password = "xterm17";
+
+  $conn = new mysqli($servername, $username, $password);
+
+  if ($conn -> connect_error) {
+    die("Connection failed: " . $conn -> connect_error);
+  }
+  echo "Connected successfuly";
+
+  $sql = "SELECT id, name, length FROM oku";
+  $result = $conn -> query($sql);
+
+  echo $result;
+?>
