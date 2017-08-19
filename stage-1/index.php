03e2e0bc2daae7526b44fdd75635b8667901ae98
johnotu <johnne01@gmail.com>
2017-08-19 17:37:56 +0000

    Added dbname variable to correct errors Changes to be committed: 	modified:   index.php

--- stage-1/index.php
+++ stage-1/index.php
@@ -2,8 +2,9 @@
   $servername = "localhost:3306";
   $username = "johnotu";
   $password = "xterm17";
+  $dbname = "streets";
 
-  $conn = new mysqli($servername, $username, $password);
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
   if ($conn -> connect_error) {
     die("Connection failed: " . $conn -> connect_error);
